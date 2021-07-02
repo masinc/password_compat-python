c08ce373883b0da718ece375b3b13d420ad9ab96
masinc <masinc000@gmail.com>
2021-07-02 23:09:18 +0900

    support php 5.3 and 5.4

---
+++ old-php/password_verify.php
@@ -0,0 +1,10 @@
+<?php
+require_once('vendor/autoload.php');
+
+$password = $argv[1];
+$hashed_password = $argv[2];
+
+$result = password_verify($password, $hashed_password);
+if (!$result) {
+    exit(1);
+}
---
+++ old-php/test_compare_old_php.py
@@ -0,0 +1,30 @@
+import unittest
+import subprocess
+
+import password_compat
+
+class TestCompareOldPhp(unittest.TestCase):
+    def test_php_password_hash(self):
+        passwords = [
+            "foo", "bar", "baz", "hoge", "fuga"
+        ]
+
+        for password in passwords:
+            result = subprocess.run(["php", "php/password_hash.php", password], check=True, stdout=subprocess.PIPE)
+            hashed_password: bytes = result.stdout
+            hashed_password: str = hashed_password.decode().strip()
+
+            self.assertTrue(password_compat.password_verify(password, hashed_password))
+
+
+    def test_php_password_verify(self):
+        passwords = [
+            "foo", "bar", "baz", "hoge", "fuga"
+        ]
+
+        for password in passwords:
+            hashed_password = password_compat.password_hash(password)
+
+            result = subprocess.run(["php", "php/password_verify.php", password, hashed_password], check=True)
+            self.assertEqual(result.returncode, 0)
+
