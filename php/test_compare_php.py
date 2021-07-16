900a993fd0c76174dbf0e2a58ea6fc070b735731
masinc <masinc000@gmail.com>
2021-07-17 04:57:10 +0900

    rafactor

--- php/test_compare_php.py
+++ php/test_compare_php.py
@@ -3,6 +3,7 @@ import subprocess
 
 import password_compat
 
+
 class TestComparePhp(unittest.TestCase):
     def test_php_password_hash(self):
         passwords = [
@@ -10,12 +11,13 @@ class TestComparePhp(unittest.TestCase):
         ]
 
         for password in passwords:
-            result = subprocess.run(["php", "php/password_hash.php", password], check=True, stdout=subprocess.PIPE)
+            result = subprocess.run(
+                ["php", "php/password_hash.php", password], check=True, stdout=subprocess.PIPE)
             hashed_password: bytes = result.stdout
             hashed_password: str = hashed_password.decode().strip()
 
-            self.assertTrue(password_compat.password_verify(password, hashed_password))
-
+            self.assertTrue(password_compat.password_verify(
+                password, hashed_password))
 
     def test_php_password_verify(self):
         passwords = [
@@ -25,6 +27,6 @@ class TestComparePhp(unittest.TestCase):
         for password in passwords:
             hashed_password = password_compat.password_hash(password)
 
-            result = subprocess.run(["php", "php/password_verify.php", password, hashed_password], check=True)
+            result = subprocess.run(
+                ["php", "php/password_verify.php", password, hashed_password], check=True)
             self.assertEqual(result.returncode, 0)
-
